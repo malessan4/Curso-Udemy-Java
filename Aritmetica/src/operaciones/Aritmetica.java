92c4609fb3b517ef717f7aa89dee29dca66ab969
Matias-Alessandrello <matialess@gmail.com>
2024-05-03 15:43:48 -0300

    metodos, continua

--- Aritmetica/src/operaciones/Aritmetica.java
+++ Aritmetica/src/operaciones/Aritmetica.java
@@ -1,13 +1,21 @@
 package operaciones;
 
 public class Aritmetica {
+
     //Atributos de la clase
     int a;
     int b;
-    
+
     //Metodo
-    public void sumar(){
+    public void sumar() {
         int resultado = a + b;
         System.out.println("Resultado = " + resultado);
+    }
+
+    public int sumarConRetorno(){
+       int resultado = a + b;
+       return resultado;
+
+
     }
 }
