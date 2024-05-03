8bff26ee4c0d401bc7c31afa788c187274ab3689
Matias-Alessandrello <matialess@gmail.com>
2024-05-03 09:10:27 -0300

    clases

---
+++ JavaApplication4/src/clases/Persona.java
@@ -0,0 +1,10 @@
+
+package clases;
+
+public class Persona {
+    
+    public static void main(String[] args) {
+        psvm
+    }
+    
+}
---
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,12 @@
+
+package javaapplication4;
+
+
+public class JavaApplication4 {
+
+
+    public static void main(String[] args) {
+
+    }
+    
+}
