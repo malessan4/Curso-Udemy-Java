8efc338c7c3a35d798ce778a3acb843082969e9e
Matias-Alessandrello <matialess@gmail.com>
2024-05-03 14:55:17 -0300

    metodos

---
+++ Aritmetica/src/operaciones/Aritmetica.java
@@ -0,0 +1,13 @@
+package operaciones;
+
+public class Aritmetica {
+    //Atributos de la clase
+    int a;
+    int b;
+    
+    //Metodo
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("Resultado = " + resultado);
+    }
+}
---
+++ Aritmetica/src/operaciones/PruebaAritmetica.java
@@ -0,0 +1,9 @@
+
+package operaciones;
+
+public class PruebaAritmetica {
+    public static void main(String[] args) {
+        
+    }
+    
+}
