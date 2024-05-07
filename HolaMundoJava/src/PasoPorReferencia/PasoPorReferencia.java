7bf8eb21cd1d6f49c6bc5d3f55340ba9011fe69d
Matias-Alessandrello <matialess@gmail.com>
2024-05-07 08:23:55 -0300

    seccion 13

---
+++ HolaMundoJava/src/PasoPorReferencia/PasoPorReferencia.java
@@ -0,0 +1,9 @@
+package PasoPorReferencia;
+
+import clases.Persona;
+
+public class PasoPorReferencia {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+    }
+}
--- HolaMundoJava/src/pasoporvalor/PasoPorValor.java
+++ HolaMundoJava/src/pasoporvalor/PasoPorValor.java
@@ -10,9 +10,10 @@ public class PasoPorValor {
         System.out.println("x nuevo valor = " + x);
     }
 
+    // se pone int en vez de void porq devuelve algo, este caso un entero
     public static void cambioValor(int arg1){
         System.out.println("arg1 = " + arg1);
-        arg1 = 15;
+         arg1 = 15;
 
     }
 }
